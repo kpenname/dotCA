165fd7e5b60ffa80bbff4af8395be8b9034ff360
Karen Fahey <61699872+kpenname@users.noreply.github.com>
2020-06-20 08:23:50 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html:5>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Karen Fahey Business Solutions</title>        
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <header><div><h2>Welcome to Karen Fahey Business Solutions</h2></div></header> 
+        <main id="main"><div><h3>The website is currently under construction, please stay tuned</h2></div></main>
+        <footer><p>&copy;2020 Karen Fahey</p></footer>        
+    </body>
+</html>
